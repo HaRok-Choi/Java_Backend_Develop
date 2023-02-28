7c45629ff7d6b2861cef96ecd18066f3ab227292
harok <gkfhrwkd23@naver.com>
2023-02-28 11:07:12 +0900

    Use of Java language

--- src/example/SignIn/SignIn.java
+++ src/example/SignIn/SignIn.java
@@ -20,7 +20,7 @@ public class SignIn {
 //		2. 사용자가 모두 (아이디와 비밀번호) 입력했는지 검증
 //		userId, userPassword 변수가 비었는지 확인
 //		문자열에서 값이 비었는지 확인하는 기능 (메서드)
-//		string.isEmpty(): 비었으면 true, 아니면 false
+//		string.isEmpty(): 비었으면 true, 아니면 false, 공백도 입력 했다고 생각함.
 		if (userId.isEmpty() || userPassword.isEmpty()) {
 			System.out.println("모두 입력하세요.");
 			return;	// 그냥 스레드 자체를 종료시킬려면 return 치면 됨
@@ -28,10 +28,11 @@ public class SignIn {
 		
 //		3. 사용자 입력값과 저장된 값이 같은지 검증 -> 검증한다고 하면 if 걸어야 됨
 //		string.equals(비교대상): 같으면 true, 아니면 false
-		if (userId.equals(ID) && userPassword.equals(PASSWORD)) {
-			System.out.println("성공");
+		if (!(userId.equals(ID) && userPassword.equals(PASSWORD))) {
+			System.out.println("로그인 정보가 일치하지 않습니다.");
+			return;
 		}
-		
+		System.out.println("성공");	// '성공'을 밖으로 빼는 것이 프로세스상 맞다.
 		
 		
 	}
