032cceda10d24941e4176928063c33929a1f825e
Harok Choi <gkfhrwkd23@naver.com>
2023-03-03 13:20:18 +0900

    Java

--- src/chapter03/B_VariableMethod/Method2.java
+++ src/chapter03/B_VariableMethod/Method2.java
@@ -1,6 +1,5 @@
 package chapter03.B_VariableMethod;
 
-
 class Triangle {
 	double underside;
 	double height;
@@ -11,22 +10,34 @@ class Triangle {
 
 class TriangleMath {
 	
+//	파라미터로 참조변수를 받는다면 메모리 주소가 복사되서 넘어오기 때문에 실제 작업도 해당 메모리 주소에서 작업을 진행함
+//	실제 데이터가 변경됨
+	
 	static void setArea(Triangle triangle) {
 		double area = 0.5 * triangle.underside * triangle.height;
 		triangle.area = area;
+	}
+	
+	static void setDiagonal(Triangle triangle) {
+		double squareDiagonal = Math.pow(triangle.underside, 2);
+		double diagonal = Math.sqrt(squareDiagonal);
+		triangle.diagonal = diagonal;
+	}
+	
+	static void setCircumference(Triangle triangle) {
+		triangle.circumference = triangle.underside + triangle.height + triangle.diagonal;
 	}
 	
 	static Triangle createTriangle(double underside, double height) {
 		Triangle triangle = new Triangle();
 		triangle.underside = underside;
 		triangle.height = height;
-		triangle.diagonal = Math.sqrt(Math.pow(underside, 2) + Math.pow(height, 2));
+		setDiagonal(triangle);
 		setArea(triangle);
-		triangle.circumference = triangle.underside + triangle.height + triangle.diagonal;
+		setCircumference(triangle);
 		return triangle;
 	}
 }
-
 
 public class Method2 {
 	
