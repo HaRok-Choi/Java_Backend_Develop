1a7f3f8c3218ee02d39f81ed3dfc87046ce4de77
Harok Choi <gkfhrwkd23@naver.com>
2023-03-08 12:48:27 +0900

    Java Example

---
+++ src/example/exchange/ExchangeApplication.java
@@ -0,0 +1,93 @@
+package example.exchange;
+
+import java.util.Scanner;
+
+// 환전기 어플리케이션
+// Input : 금액, 넣을 화폐, 바꿀 화폐
+// Output : 금액, 바뀐 화폐
+
+public class ExchangeApplication {
+	
+	private static String[] managedCurrencies = {
+		"KRW", "USD", "JPY"
+	};
+	
+	private static ExchangeRate[] exchangeRates = {
+//		원화 - 달러
+			new ExchangeRate("KRW", "USD", 0.76 / 1000),
+//		원화 - 엔
+			new ExchangeRate("KRW", "JPY", 104.11 / 1000),
+//		달러 - 원화
+			new ExchangeRate("USD", "KRW", 1322.50 / 1),
+//		달러 - 엔
+			new ExchangeRate("USD", "JPY", 137.68 / 1),
+//		엔 - 원화
+			new ExchangeRate("JPY", "KRW", 1000 / 104.11),
+//		엔 - 달러
+			new ExchangeRate("JPY", "USD", 0.76 / 104.11),
+	};
+	
+	public static void main(String[] args) {
+		
+//		입력
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("넣을 화폐 : ");
+		String exchangingCurrency = scanner.nextLine();
+		System.out.print("바꿀 화폐 : ");
+		String exchangedCurrency = scanner.nextLine();
+		System.out.print("금액 : ");
+		int amount = scanner.nextInt();
+		
+//		입력 검증
+//		모두 입력했는지
+		if (exchangingCurrency.isBlank() || exchangedCurrency.isBlank()) {
+			System.out.println("모두 입력해주세요.");
+			return;
+		}
+		
+//		입력한 화폐가 동일 할 때
+		if (exchangingCurrency.equals(exchangedCurrency)) {
+			System.out.println("동일한 화폐로 변경할 수 없습니다.");
+		}
+		
+//		유용한 금액이 아닐 때 (금액이 양수가 아닐 때)
+		if (amount <= 0) {
+			System.out.println("유효한 금액이 아닙니다.");
+		}
+		
+//		관리하고있는 화폐 단위가 아닐 때
+		
+		boolean hasExchanging = false;
+		boolean hasExchanged = false;
+		
+		for (String managedCurrency : managedCurrencies) {
+			if (exchangingCurrency.equals(managedCurrency)) {
+				hasExchanging = true;
+			}
+			if (exchangedCurrency.equals(managedCurrency)) {
+				hasExchanged = true;
+			}
+		}
+		
+		if (!(hasExchanging && hasExchanged)) {
+			System.out.println("유효하지 않은 화폐 단위입니다.");
+			return;
+		}
+		
+		double resultAmount = amount;
+		
+		for (ExchangeRate exchangeRate : exchangeRates) {
+			
+			boolean isSame = exchangingCurrency.equals(exchangeRate.getExchangingCurrency()) && exchangedCurrency.equals(exchangeRate.getExchangedCurrency());
+			if (isSame) {
+				resultAmount *= exchangeRate.getExchangeRate();
+				break;	// 만족하면 프로세스가 종료 될 수 있게끔(메모리 낭비 방지)
+			}
+		}
+		
+//		정상 출력
+		System.out.println(exchangedCurrency + ": " + resultAmount);
+		
+	}
+}
